0f16428b92de2d04478b839930f09f02404900a9
sch804 <sch804@pukyong.ac.kr>
2020-09-24 11:32:25 +0000

    fixed solution1

--- 8/chapter8-2.py
+++ 8/chapter8-2.py
@@ -6,9 +6,10 @@ a[2]=1
 for i in range(3,x+1):
   a[i]=a[i-1]+1
   if i%5==0:
-    a[i]=min(a[i],a[i//5])
+    a[i]=min(a[i],a[i//5]+1)
   if i%3==0:
-    a[i]=min(a[i],a[i//3])
+    a[i]=min(a[i],a[i//3]+1)
   if i%2==0:
-    a[i]=min(a[i],a[i//2])
+    a[i]=min(a[i],a[i//2]+1)
+
 print(a[x])
