24b49491a18bcd10fd5a6975771a339915076d90
sch804 <sch804@pukyong.ac.kr>
2020-09-27 11:37:04 +0000

    fix chapter5-3.py

--- 5/chapter5-3.py
+++ 5/chapter5-3.py
@@ -3,10 +3,12 @@ import sys
 #n,m입력받기
 n,m=map(int,sys.stdin.readline().split())
 
+#리스트 입력받기
 d=[]
 for i in range(n):
   d.append(list(map(int,sys.stdin.readline().split())))
 
+#dfs 만들기
 def dfs(x,y):
   if x>=0 and x<n and y>=0 and y<m:
     
@@ -22,6 +24,7 @@ def dfs(x,y):
   else:
     return 0
 
+#얼음덩어리 개수 세기
 count=0
 for i in range(n):
   for j in range(m):
